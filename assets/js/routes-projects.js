2d4b6b8544d7982227f64e70fc92ba5e559ae094
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-12-02 22:48:25 -0300

    feat: adicionando escolpo das rotas do protifólio

---
+++ assets/js/routes-projects.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const bookcase = document.querySelector("#project-bookcase");
+    const movieStore = document.querySelector("#project-movie-store");
+    const mario = document.querySelector("#project-mario");
+    const businessCard = document.querySelector("#project-business-card");
+
+    bookcase.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/', '_blank');
+    });
+
+    movieStore.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/', '_blank');
+    });
+
+    mario.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/', '_blank');
+    });
+
+    businessCard.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/', '_blank');
+    });
+});
