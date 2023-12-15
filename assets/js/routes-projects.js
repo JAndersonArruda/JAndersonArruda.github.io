fdf4ada8ed8616c10388f0b8ca5ddae5a639eb25
Jose Anderson Arruda <ferrazarrudaanderson@gmail.com>
2023-12-15 14:38:28 -0300

    feat: adicionando rotas para repositorios github dos projetos

--- assets/js/routes-projects.js
+++ assets/js/routes-projects.js
@@ -1,22 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const bookcase = document.querySelector("#project-bookcase");
-    const movieStore = document.querySelector("#project-movie-store");
-    const mario = document.querySelector("#project-mario");
+    const pokedex = document.querySelector("#project-pokedex");
+    const distributionGraph = document.querySelector("#project-distribution-graph");
+    const script = document.querySelector("#project-script");
+    const gameList = document.querySelector("#project-game-list");
+    const flaskRecipe = document.querySelector("#project-flask-recipe");
+    const carinaWeb = document.querySelector("#project-carina-web");
     const businessCard = document.querySelector("#project-business-card");
+    const mario = document.querySelector("#project-mario");
+    const movieStore = document.querySelector("#project-movie-store");
+    const calEa = document.querySelector("#project-calEa");
+    const bookcase = document.querySelector("#project-bookcase");
 
-    bookcase.addEventListener("click", () => {
-        window.open('https://github.com/JAndersonArruda/', '_blank');
+    pokedex.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/projeto-final-cleanbug-PokeDex', '_blank');
     });
 
-    movieStore.addEventListener("click", () => {
-        window.open('https://github.com/JAndersonArruda/', '_blank');
+    distributionGraph.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/Probability-Distribution-Graph', '_blank');
+    })
+
+    script.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/Script-Laguege-Activity', '_blank');
     });
 
-    mario.addEventListener("click", () => {
-        window.open('https://github.com/JAndersonArruda/', '_blank');
+    gameList.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/LOOPIS-Game-List', '_blank');
+    });
+
+    flaskRecipe.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/Flask-Receita', '_blank');
+    });
+
+    carinaWeb.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/CarinaWebb', '_blank');
     });
 
     businessCard.addEventListener("click", () => {
-        window.open('https://github.com/JAndersonArruda/', '_blank');
+        // window.open('', '_blank');
+    });
+
+    mario.addEventListener("click", () => {
+        // window.open('', '_blank');
+    });
+    
+    movieStore.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/Locadora-Filmes', '_blank');
+    });
+    
+    calEa.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/CalEA', '_blank');
+    });
+
+    bookcase.addEventListener("click", () => {
+        window.open('https://github.com/JAndersonArruda/Estante-Livros', '_blank');
     });
 });
